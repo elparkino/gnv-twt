0e06158ad51f94c95bc81e9777367525e28f3271
Parker <parker.emailaddress@gmail.com>
2013-08-10 19:00:37 -0400

    A little layout to error.php

--- error.php
+++ error.php
@@ -2,15 +2,22 @@
 <head>
 	<title>You Fucked up</title>
 </head>
-1 <!-- Latest compiled and minified CSS -->
-2 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
-3 
-4 <!-- Latest compiled and minified JavaScript -->
-5 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
 <script>
 
 </script>
 <body>
-	<a href="gnv/connect.php"><button class="btn btn-danger">You fucked up</button></a>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				<a href="gnv/connect.php"><button class="btn btn-danger">You fucked up</button></a>	
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
