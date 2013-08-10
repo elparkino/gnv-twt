6eb85fa4909d4093ffb3d417193f1a6f9dbc8009
Parker <parker.emailaddress@gmail.com>
2013-08-10 19:43:58 -0400

    I need a guilt free bj

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-	require ("twitter_oauth.php");
-	echo $result['id']; 
-	echo $result['username']; 
-	echo $result['oauth_uid']; 
-	echo $result['oauth_provider']; 
-	echo $result['oauth_token']; 
-	echo $result['oauth_secret']; 
+require("admin/twitter-db.php");
+require("twitteroauth/twitteroauth.php");  
+
+    	echo $_SESSION['id'];
+    	echo $_SESSION['username'];
+    	echo $_SESSION['oauth_uid'];
+    	echo $_SESSION['oauth_provider'];
+    	echo $_SESSION['oauth_token'];
+    	echo $_SESSION['oauth_secret'];
 ?>
