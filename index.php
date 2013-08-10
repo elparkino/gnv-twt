aadf4251da264742184743edc1c63177c1bc3410
Parker <parker.emailaddress@gmail.com>
2013-08-10 18:47:02 -0400

    added index.php, and if all goes to plan, we';; be talking to the global results variable that stures the info in the DB

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+	require ("twitter_oauth.php");
+	echo $result['id']; 
+	echo $result['username']; 
+	echo $result['oauth_uid']; 
+	echo $result['oauth_provider']; 
+	echo $result['oauth_token']; 
+	echo $result['oauth_secret']; 
+?>
