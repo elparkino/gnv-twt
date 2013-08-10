74942f5c344824beb92e3e7872e9e780bb901d50
Parker <parker.emailaddress@gmail.com>
2013-08-10 19:46:35 -0400

    Still redirect loop, but now in index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php 
 require("admin/twitter-db.php");
 require("twitteroauth/twitteroauth.php");  
-
-    	echo $_SESSION['id'];
-    	echo $_SESSION['username'];
-    	echo $_SESSION['oauth_uid'];
-    	echo $_SESSION['oauth_provider'];
-    	echo $_SESSION['oauth_token'];
-    	echo $_SESSION['oauth_secret'];
+	print_r($_SESSION);
 ?>
