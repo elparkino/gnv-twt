60f7210171726ae4d95dd0254ea9d7326e18e69a
Parker <parker.emailaddress@gmail.com>
2013-08-10 18:20:05 -0400

    db connection script added, and sql query to oauth.php

--- twitter_oauth.php
+++ twitter_oauth.php
@@ -1,4 +1,5 @@
 <?php 
+require("admin/twitter-db.php");
 require("twitteroauth/twitteroauth.php");  
 session_start(); 
 
@@ -14,7 +15,35 @@ if (!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empt
 	$user_info = $twitteroauth->get('account/verify_credentials'); 
 	// Print user's info  
 	print_r($user_info);  
-	
+
+	if(isset($user_info->error)){  
+    // Something's wrong, go back to square 1  
+    header('Location: connect.php'); 
+	} else { 
+    // Let's find the user by its ID  
+    $query = mysql_query("SELECT * FROM users WHERE oauth_provider = 'twitter' AND oauth_uid = ". $user_info->id);  
+    $result = mysql_fetch_array($query);  
+  
+    // If not, let's add it to the database  
+    if(empty($result)){  
+        $query = mysql_query("INSERT INTO users (oauth_provider, oauth_uid, username, oauth_token, oauth_secret) VALUES ('twitter', {$user_info->id}, '{$user_info->screen_name}', '{$access_token['oauth_token']}', '{$access_token['oauth_token_secret']}')");  
+        $query = mysql_query("SELECT * FROM users WHERE id = " . mysql_insert_id());  
+        $result = mysql_fetch_array($query);  
+    } else {  
+        // Update the tokens  
+        $query = mysql_query("UPDATE users SET oauth_token = '{$access_token['oauth_token']}', oauth_secret = '{$access_token['oauth_token_secret']}' WHERE oauth_provider = 'twitter' AND oauth_uid = {$user_info->id}");  
+    }  
+  
+    $_SESSION['id'] = $result['id']; 
+    $_SESSION['username'] = $result['username']; 
+    $_SESSION['oauth_uid'] = $result['oauth_uid']; 
+    $_SESSION['oauth_provider'] = $result['oauth_provider']; 
+    $_SESSION['oauth_token'] = $result['oauth_token']; 
+    $_SESSION['oauth_secret'] = $result['oauth_secret']; 
+ 
+    header('Location: connect.php');  
+}  
+
 }else{
 	header('Location: connect.php');
 }
