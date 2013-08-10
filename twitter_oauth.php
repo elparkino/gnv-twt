b89604726bce36b40fd0a0b4768f8b30f45b900e
Parker <parker.emailaddress@gmail.com>
2013-08-10 19:26:57 -0400

    My brain is hurting

--- twitter_oauth.php
+++ twitter_oauth.php
@@ -18,7 +18,7 @@ if (!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empt
 
 	if(isset($user_info->error)){  
     	// Something's wrong, go back to square 1  
-    	header('Location: error-p.php'); 
+    	header('Location: index.php'); 
 	} else { 
     	// Let's find the user by its ID  
     	$query = mysql_query("SELECT * FROM users WHERE oauth_provider = 'twitter' AND oauth_uid = ". $user_info->id);  
